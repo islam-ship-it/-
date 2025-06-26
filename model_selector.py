716ea694d5946a5c009ae1a40659d922b9a8affd
islam-ship-it <islamtamersa3ed@gmail.com>
2025-06-26 07:34:18 +0300

    Update model_selector.py

--- model_selector.py
+++ model_selector.py
@@ -1,5 +1,5 @@
 # model_selector.py
 
-def choose_model(message, matched_services):
-    # استخدم النموذج المدرب المخصص فقط في كل الحالات
-    return "ft:gpt-3.5-turbo-1106:waaatsaaap:BmHlxioXckpt-step-161", "Model: المتدرب"
+#def choose_model(message, matched_services):
+#    # استخدم النموذج المدرب المخصص فقط في كل الحالات
+#    return "ft:gpt-3.5-turbo-1106:waaatsaaap:BmHlxioXckpt-step-161", "Model: المتدرب"
