e6b99c0fd76d9d318263ecc4d958e6df6fa1e5d8
islam-ship-it <islamtamersa3ed@gmail.com>
2025-07-07 02:40:31 +0300

    Create config.py

---
+++ config.py
@@ -0,0 +1,11 @@
+import os
+
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+OPENROUTER_API_KEY = os.getenv("OPENROUTER_API_KEY")
+OPENROUTER_API_BASE = "https://openrouter.ai/api/v1"
+ZAPI_BASE_URL = os.getenv("ZAPI_BASE_URL")
+ZAPI_INSTANCE_ID = os.getenv("ZAPI_INSTANCE_ID")
+ZAPI_TOKEN = os.getenv("ZAPI_TOKEN")
+CLIENT_TOKEN = os.getenv("CLIENT_TOKEN")
+ASSISTANT_ID = os.getenv("ASSISTANT_ID")
+MONGO_URI = os.getenv("MONGO_URI")
